ee03c66f9abd45bf461ba064a9101f522b542192
Zach White <zechnwhite@gmail.com>
2019-10-24 23:02:59 -0700

    got the files working together a bit now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './css/App.css';
-import Form from './components/Form';
+import './css/App/App.css';
+import Form from './components/Form'
 
 function App() {
   return (
---
+++ src/components/Form.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Form extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state ={
+      display: true
+    }
+
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>hello</h1>
+      </div>
+    )
+  }
+}
+
+export default Form;
