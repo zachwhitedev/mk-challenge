b262d3251b8c5b38f5163ec78664c3030075abbe
Zach White <zechnwhite@gmail.com>
2019-10-24 23:33:31 -0700

    adds a beginner state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 class Form extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            name: '',
+            email: '',
+            message: '',
+        }
+    }
   render() {
+
+
     return (
       <div>
           <h1>hello</h1>
