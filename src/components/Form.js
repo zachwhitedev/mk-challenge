fb33b80299174403e1c9339572f719301442fd1c
Zach White <zechnwhite@gmail.com>
2019-10-25 00:27:34 -0700

    progress on form UI. installed material/styles

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Logo from '../assets/mk-logo.png';
 
 class Form extends React.Component {
     constructor(props){
         super(props);
 
         this.state = {
-            name: '',
+            name: 'Please enter your name.',
             email: '',
             message: '',
         }
@@ -18,13 +21,34 @@ class Form extends React.Component {
         this.setState({[e.target.name]: e.target.value})
     }
   render() {
+    const classes = useStyles();
 
     return (
-      <div className='form'>
-          <TextField /><br></br>
-          <TextField /><br></br>
-          <TextField /><br></br>
+        <div className='container'>
+         <div className='form'>
+            <img src={Logo} alt='MK Decision company logo.'/>
+            <TextField 
+                className='input'
+                id='standard-name'
+                label='name'
+            />
+            <TextField 
+                className = 'input'
+                label='email'    
+            />
+            <TextField 
+                className='input'
+                label='message'
+                multiline={true}
+                rows={2}
+                rowsMax={4}
+            />
+            <Button variant="contained" className={classes.button}>
+                Submit
+            </Button>
+        </div>
       </div>
+
     )
   }
 }
